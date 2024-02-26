62bc3f13020ec55b0729154ff268694f6f172692
BibekBhusal0 <138126735+BibekBhusal0@users.noreply.github.com>
2024-02-26 18:58:28 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+
+function Square({ val }) {
+  const [value, setValue] = useState(val.toUpperCase());
+
+  function handleClick() {
+    setValue(value.toUpperCase() === "X" ? "O" : "X");
+  }
+  return (
+    <>
+      <button className="square" onClick={handleClick}>
+        {value}
+      </button>
+    </>
+  );
+}
+
+function Board() {
+  const [board, setBoard] = useState(Array(9).fill(""));
+  return (
+    <div className="board">
+      {[0, 1, 2].map((row) => {
+        return (
+          <div key={row} className="row">
+            {[0, 1, 2].map((col) => {
+              const cell = row * 3 + col;
+              return <Square key={cell} val={board[cell]} />;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board />
     </div>
   );
 }
