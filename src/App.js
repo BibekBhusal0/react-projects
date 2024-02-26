8ba1abb173bab8ce25fc92e23bd684f272878e67
BibekBhusal0 <138126735+BibekBhusal0@users.noreply.github.com>
2024-02-26 21:27:35 +0545

    logic complete

--- src/App.js
+++ src/App.js
@@ -1,37 +1,79 @@
 import "./App.css";
-import React from "react";
 import { useState } from "react";
 
-function Square({ val }) {
-  const [value, setValue] = useState(val.toUpperCase());
-
-  function handleClick() {
-    setValue(value.toUpperCase() === "X" ? "O" : "X");
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
+  return null;
+}
+
+function Square({ value, clickFunction }) {
   return (
-    <>
-      <button className="square" onClick={handleClick}>
-        {value}
-      </button>
-    </>
+    <div className="square" onClick={clickFunction}>
+      <span className={value}>{value}</span>
+    </div>
   );
 }
 
 function Board() {
   const [board, setBoard] = useState(Array(9).fill(""));
+  const [x, changeTurn] = useState(true);
+  const [winner, changeWinner] = useState(null);
+
+  const handleClick = (index) => {
+    if (board[index] === "" && !winner) {
+      const temp_board = [...board];
+      temp_board[index] = x ? "X" : "O";
+      changeTurn(!x);
+      setBoard(temp_board);
+      changeWinner(calculateWinner(temp_board));
+    }
+  };
+
+  const reset = () => {
+    setBoard(Array(9).fill(""));
+    changeWinner(null);
+  };
+
   return (
-    <div className="board">
-      {[0, 1, 2].map((row) => {
-        return (
-          <div key={row} className="row">
-            {[0, 1, 2].map((col) => {
-              const cell = row * 3 + col;
-              return <Square key={cell} val={board[cell]} />;
-            })}
-          </div>
-        );
-      })}
-    </div>
+    <main>
+      <div className="info">
+        {winner ? `${winner} wins` : `${x ? "X" : "O"}'s turn`}
+      </div>
+      <div className="board">
+        {[0, 1, 2].map((row) => {
+          return (
+            <div key={row} className="row">
+              {[0, 1, 2].map((col) => {
+                const cell = row * 3 + col;
+                return (
+                  <Square
+                    key={cell}
+                    clickFunction={() => handleClick(cell)}
+                    value={board[cell]}
+                  />
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+      <button onClick={reset}>Reset </button>
+    </main>
   );
 }
 
